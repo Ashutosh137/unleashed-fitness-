dae7a2f6d94f2495e46d6c9beb307e4399fa5d87
ashutosh sharma <mr.luckysharma7@gmail.com>
2023-11-01 23:14:18 +0530

    .

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -5,20 +5,20 @@ const Navbar = () => {
         <div>
             <nav className="navbar py-3 navbar-expand-lg text-capitalize text-white">
                 <div className="container-fluid">
-                    <label className='title' htmlFor=""><Link to='/' className="navbar-brand text-center fw-bold h1">Unleashed Fitness</Link></label>
+                   <Link to='/' className="navbar-brand text-center"> <h1 className='title' htmlFor="">Unleashed Fitness</h1></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav text-center h5  m-auto mb-2 mb-lg-0">
-                            <li className="nav-item m-auto mx-1">
-                                <Link to="/" className="nav-link active h4" aria-current="page">Home</Link>
+                        <ul className="navbar-nav text-center h4  m-auto mb-2 mb-lg-0">
+                            <li className="nav-item m-auto  mx-1">
+                                <Link to="/" className="nav-link active h3" aria-current="page"><h4>Home</h4></Link>
                             </li>
                             <li className="nav-item m-auto mx-1">
-                                <Link to="/allexercises" className="nav-link h4">exercises</Link>
+                                <Link to="/allexercises" className="nav-link h4"><h4>Exercise</h4></Link>
                             </li>
                             <li className="nav-item m-auto mx-1">
-                                <Link to="/favourite" className="nav-link h4">favourite</Link>
+                                <Link to="/favourite" className="nav-link h4"><h4>favourite</h4></Link>
                             </li>
                         </ul>
                     </div>
